11b317df8ca6236ff2560d5aba5b82ee29572d53
Aman-Kasa <a.kasa@alustudent.com>
2026-05-29 00:22:12 +0200

    Fix infinite include loop in heap.h

--- huffman_coding/heap/heap.h
+++ huffman_coding/heap/heap.h
@@ -1,79 +1,42 @@
-#include <stdlib.h>
-#include "heap.h"
+#ifndef HEAP_H
+#define HEAP_H
+
+#include <stddef.h>
 
 /**
- * bubble_up - Restores min-heap property by floating up a node
- * @heap: Pointer to the heap
- * @node: Pointer to the newly inserted node
- */
-void bubble_up(heap_t *heap, binary_tree_node_t *node)
-{
-	void *temp;
-
-	while (node->parent && heap->data_cmp(node->data, node->parent->data) < 0)
-	{
-		temp = node->data;
-		node->data = node->parent->data;
-		node->parent->data = temp;
-		node = node->parent;
-	}
-}
-
-/**
- * heap_insert - Inserts a value in a Min Binary Heap
- * @heap: Pointer to the heap in which the node has to be inserted
- * @data: Pointer containing the data to store in the new node
+ * struct binary_tree_node_s - Binary tree node data structure
  *
- * Return: Pointer to the created node containing data, or NULL if it fails
+ * @data: Data stored in a node
+ * @left: Pointer to the left child
+ * @right: Pointer to the right child
+ * @parent: Pointer to the parent node
  */
-binary_tree_node_t *heap_insert(heap_t *heap, void *data)
+typedef struct binary_tree_node_s
 {
-	binary_tree_node_t *new_node, *parent;
-	size_t mask, target_idx;
+	void *data;
+	struct binary_tree_node_s *left;
+	struct binary_tree_node_s *right;
+	struct binary_tree_node_s *parent;
+} binary_tree_node_t;
 
-	if (!heap || !data)
-		return (NULL);
+/**
+ * struct heap_s - Heap data structure
+ *
+ * @size: Size of the heap (number of nodes)
+ * @data_cmp: Function to compare two nodes data
+ * @root: Pointer to the root node of the heap
+ */
+typedef struct heap_s
+{
+	size_t size;
+	int (*data_cmp)(void *, void *);
+	binary_tree_node_t *root;
+} heap_t;
 
-	new_node = binary_tree_node(NULL, data);
-	if (!new_node)
-		return (NULL);
+/* Function Prototypes */
+binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data);
+heap_t *heap_create(int (*data_cmp)(void *, void *));
+binary_tree_node_t *heap_insert(heap_t *heap, void *data);
+void *heap_extract(heap_t *heap);
 
-	if (!heap->root)
-	{
-		heap->root = new_node;
-		heap->size++;
-		return (new_node);
-	}
-
-	/* target_idx represents the 1-based index of the new node */
-	target_idx = heap->size + 1;
-	parent = heap->root;
-
-	/* Find the highest bit in target_idx (e.g., for 6 [110], mask becomes 4 [100]) */
-	mask = 1;
-	while (mask <= target_idx)
-		mask <<= 1;
-	mask >>= 2; /* Shift past the highest bit since root is already found */
-
-	/* Move down the tree until we are exactly at the parent node */
-	while (mask > 1)
-	{
-		if (target_idx & mask)
-			parent = parent->right;
-		else
-			parent = parent->left;
-		mask >>= 1;
-	}
-
-	/* Connect the parent to the new node based on the last bit */
-	new_node->parent = parent;
-	if (target_idx & 1)
-		parent->right = new_node;
-	else
-		parent->left = new_node;
-
-	heap->size++;
-	bubble_up(heap, new_node);
-
-	return (new_node);
-}
+#endif /* HEAP_H */
