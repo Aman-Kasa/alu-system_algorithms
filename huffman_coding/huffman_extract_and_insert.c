7d2e57a667766ad294da84473af67f061761b737
Aman-Kasa <a.kasa@alustudent.com>
2026-05-29 18:58:38 +0200

    tree_fixed

--- huffman_coding/huffman_extract_and_insert.c
+++ huffman_coding/huffman_extract_and_insert.c
@@ -1,53 +1,20 @@
-#include <stdlib.h>
-#include "heap.h"
 #include "huffman.h"
 
 /**
- * huffman_extract_and_insert - Extracts two nodes and inserts a parent node
- * @p_queue: Pointer to the priority queue min-heap
- *
- * Return: 1 on success, 0 on failure
+ * huffman_extract_and_insert - description
+ * @priority_queue: heap_t
+ * Return: int
  */
-int huffman_extract_and_insert(heap_t *p_queue)
+int huffman_extract_and_insert(heap_t *priority_queue)
 {
-	binary_tree_node_t *first, *second, *parent;
-	symbol_t *sym1, *sym2, *parent_sym;
-
-	if (!p_queue || !p_queue->root)
-		return (0);
-
-	first = (binary_tree_node_t *)heap_extract(p_queue);
-	second = (binary_tree_node_t *)heap_extract(p_queue);
-	if (!first || !second)
-	{
-		if (first)
-			free(first->data), free(first);
-		return (0);
-	}
-
-	sym1 = (symbol_t *)first->data;
-	sym2 = (symbol_t *)second->data;
-	parent_sym = symbol_create('$', sym1->freq + sym2->freq);
-	if (!parent_sym)
-		return (0);
-
-	parent = binary_tree_node(NULL, parent_sym);
-	if (!parent)
-	{
-		free(parent_sym);
-		return (0);
-	}
-
-	parent->left = first;
-	parent->right = second;
-	first->parent = parent;
-	second->parent = parent;
-
-	if (!heap_insert(p_queue, parent))
-	{
-		free(parent_sym), free(parent);
-		return (0);
-	}
-
-	return (1);
+	node_t *nl = heap_extract(priority_queue);
+	node_t *nr = heap_extract(priority_queue);
+	symbol_t *sl = (symbol_t *)nl->data;
+	symbol_t *sr = (symbol_t *)nr->data;
+	size_t freq = sl->freq + sr->freq;
+	symbol_t *s = symbol_create(-1, freq);
+	node_t *n = nl->parent = nr->parent = binary_tree_node(NULL, s);
+	n->left = nl;
+	n->right = nr;
+	return (heap_insert(priority_queue, n) ? 1 : 0);
 }
--- huffman_coding/huffman_tree.c
+++ huffman_coding/huffman_tree.c
@@ -1,42 +1,37 @@
-#include <stdlib.h>
-#include "heap.h"
 #include "huffman.h"
+#include <stdlib.h>
 
 /**
- * huffman_tree - Builds a Huffman tree from data and frequencies
- * @data: Array of characters
- * @freq: Array containing the associated frequencies
- * @size: Size of the arrays
+ * huffman_tree - builds a Huffman tree from characters and their frequencies
+ * @data: array of characters
+ * @freq: array of frequencies (same size as data)
+ * @size: number of elements in data and freq
  *
- * Return: Pointer to the root node of the Huffman tree, or NULL on failure
+ * Return: pointer to root of Huffman tree, or NULL on failure
  */
 binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size)
 {
-	heap_t *pq = NULL;
-	binary_tree_node_t *root = NULL;
+	heap_t *q;
+	binary_tree_node_t *root;
 
 	if (!data || !freq || size == 0)
 		return (NULL);
 
-	pq = huffman_priority_queue(data, freq, size);
-	if (!pq)
+	/* create priority queue (min heap) */
+	q = huffman_priority_queue(data, freq, size);
+	if (!q)
 		return (NULL);
 
-	while (pq->root && (pq->root->left || pq->root->right))
-	{
-		if (!huffman_extract_and_insert(pq))
-		{
-			heap_delete(pq, NULL);
-			return (NULL);
-		}
-	}
+	/* repeatedly extract two smallest nodes and merge until one node remains */
+	while (q->size > 1)
+		huffman_extract_and_insert(q);
 
-	if (pq->root)
-	{
-		root = (binary_tree_node_t *)pq->root->data;
-	}
+	/* the remaining node in the heap is the root of the Huffman tree */
+	root = (binary_tree_node_t *)q->root->data;
 
-	heap_delete(pq, NULL);
+	/* free the heap structure (not the node itself) */
+	free(q->root);
+	free(q);
 
 	return (root);
 }
